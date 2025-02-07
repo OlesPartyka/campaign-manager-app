c6d13ef3e5f67dbeecd53321cac5c07defbce076
Oles Partyka <oles.partyka.29@gmail.com>
2025-02-07 10:29:43 +0100

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { CampaignListComponent } from './components/campaign-list/campaign-list.component';
-import { CampaignFormComponent } from './components/campaign-form/campaign-form.component';
-
-export const routes: Routes = [
-  { path: '', component: CampaignListComponent },
-  { path: 'campaign/new', component: CampaignFormComponent },
-  { path: 'campaign/:id', component: CampaignFormComponent },
-];
